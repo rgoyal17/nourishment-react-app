51ff1c3227da732cfc17df0174735b212b75f0bd
Rishabh Goyal <rgoyal17@uw.edu>
2024-01-15 18:28:10 -0800

    fix build

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "NourishMent",
     slug: "nourishment",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -17,7 +17,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.rgoyal.nourishment",
-      buildNumber: "3",
+      buildNumber: "4",
     },
     android: {
       adaptiveIcon: {
